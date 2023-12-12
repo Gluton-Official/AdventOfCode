8ddb0f672f7503345a5ba23b552ae18e67a05cb0
Gluton <glutonofficial@gmail.com>
2023-12-11 23:59:48 -0500

    Day 11 refactor

--- src/Day11.kt
+++ src/Day11.kt
@@ -16,49 +16,8 @@ object Day11 : AoCPuzzle() {
         """.trimIndent()),
 	)
 
-    override fun part1(input: Input): Long {
-        val expandedImage = buildList {
-            input.forEach {
-                val row = it.replace('.', ' ')
-                add(row.toMutableList())
-                if (row.isBlank()) {
-                    add(row.toMutableList())
-                }
-            }
-        }.also { image ->
-            var columnIndex = 0
-            while (columnIndex < image.first().size) {
-                var blankColumn = true
-                for (rowIndex in image.indices) {
-                    if (image[rowIndex][columnIndex] == '#') {
-                        blankColumn = false
-                        break
-                    }
-                }
-                if (blankColumn) {
-                    columnIndex++
-                    image.onEach { row ->
-                        row.add(columnIndex, ' ')
-                    }
-                }
-                columnIndex++
-            }
-        }
-
-        val galaxyPositions = buildList {
-            expandedImage.forEachIndexed { rowIndex, row ->
-                row.forEachIndexed { columnIndex, dataPoint ->
-                    if (dataPoint == '#') {
-                        add(Position(rowIndex.toLong(), columnIndex.toLong()))
-                    }
-                }
-            }
-        }
-
-        return galaxyPositions.sumOf { first ->
-            galaxyPositions.sumOf { second -> first.manhattanDistance(second) }
-        } / 2
-    }
+    override fun part1(input: Input): Long = galaxyPositions(input, scale = 2)
+        .uniquePairs { first.manhattanDistance(second) }.sum()
 
     override val part2Tests = listOf(
 		Test(82000210L, """
@@ -75,38 +34,39 @@ object Day11 : AoCPuzzle() {
         """.trimIndent()),
 	)
 
-    override fun part2(input: Input): Long {
-        val scale = 1_000_000L
-        val galaxyPositions = mutableListOf<Position>()
-        val emptyRowIndices = mutableListOf<Int>()
-        val emptyColumnIndices = mutableListOf<Int>()
-        input.first().indices.forEach { columnIndex ->
-            val emptyColumn = input.indices.none { rowIndexAgain -> input[rowIndexAgain][columnIndex] == '#' }
-            if (emptyColumn) {
-                emptyColumnIndices += columnIndex
-            }
-        }
-        input.forEachIndexed { rowIndex, row ->
-            var emptyRow = true
-            val verticalSpace = emptyRowIndices.size
-            row.forEachIndexed { columnIndex, dataPoint ->
-                if (dataPoint == '#') {
-                    val horizontalSpace = emptyColumnIndices.count { it < columnIndex }
-                    galaxyPositions += Position(
-                        rowIndex - verticalSpace + scale * verticalSpace,
-                        columnIndex - horizontalSpace + scale * horizontalSpace
+    override fun part2(input: Input): Long = galaxyPositions(input, scale = 1_000_000)
+        .uniquePairs { first.manhattanDistance(second) }.sum()
+
+    private fun galaxyPositions(input: Input, scale: Long): List<Position> {
+        val voidRows = voidRows(input)
+        val voidColumns = voidColumns(input)
+        return input.flatMapIndexed { rowIndex, row ->
+            val verticalVoids = voidRows.count { it < rowIndex }
+            row.mapIndexedNotNull { columnIndex, dataPoint ->
+                val horizontalVoids = voidColumns.count { it < columnIndex }
+                when (dataPoint) {
+                    '#' -> Position(
+                        row = rowIndex - verticalVoids + scale * verticalVoids,
+                        column = columnIndex - horizontalVoids + scale * horizontalVoids,
                     )
-                    emptyRow = false
+                    else -> null
                 }
             }
-            if (emptyRow) {
-                emptyRowIndices += rowIndex
+        }
+    }
+
+    private fun voidRows(input: Input): List<Int> = buildList {
+        input.forEachIndexed { rowIndex, row ->
+            if ('#' !in row) add(rowIndex)
+        }
+    }
+
+    private fun voidColumns(input: Input): List<Int> = buildList {
+        input.indices.forEach { columnIndex ->
+            if (input.none { row -> row[columnIndex] == '#' }) {
+                add(columnIndex)
             }
         }
-
-        return galaxyPositions.sumOf { first ->
-            galaxyPositions.sumOf { second -> first.manhattanDistance(second) }
-        } / 2
     }
 
     private data class Position(val row: Long, val column: Long) {
