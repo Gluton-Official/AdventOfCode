475ae751d61587d9277d240e900bd9fc4173f195
Gluton <glutonofficial@gmail.com>
2024-11-30 23:41:59 -0500

    Update kotlin and dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,32 @@
 import org.jetbrains.kotlin.gradle.dsl.KotlinVersion
-import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.gradle.internal.config.LanguageFeature
 
 plugins {
-    kotlin("jvm") version "1.9.20"
+    kotlin("jvm") version "2.1.0"
 }
 
 dependencies {
     implementation("org.jetbrains.kotlin:kotlin-script-runtime")
-    implementation("org.jetbrains.kotlinx:kotlinx-io-core:0.3.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-io-core:0.5.1")
     implementation("org.jetbrains.kotlinx:kotlinx-datetime:0.5.0")
     implementation("io.github.cdimascio:dotenv-kotlin:6.4.1")
-    val ktorVersion = "2.3.6"
+    val ktorVersion = "3.0.1"
     implementation("io.ktor:ktor-client-core:$ktorVersion")
     implementation("io.ktor:ktor-client-cio:$ktorVersion")
-    implementation("com.github.ajalt.mordant:mordant:2.2.0")
+    implementation("com.github.ajalt.mordant:mordant:2.7.0")
     implementation(kotlin("test"))
 }
 
-tasks.withType<KotlinCompile> {
-    kotlinOptions.apply {
-        languageVersion = KotlinVersion.KOTLIN_2_1.version
-        apiVersion = KotlinVersion.KOTLIN_2_1.version
-    }
-}
+kotlin {
+    sourceSets.all {
+        languageSettings {
+            languageVersion = KotlinVersion.KOTLIN_2_2.version
+            apiVersion = KotlinVersion.KOTLIN_2_2.version
 
-sourceSets {
-    main {
-        kotlin.srcDir("src")
-        resources.srcDir("resources")
+            enableLanguageFeature(LanguageFeature.WhenGuards.name)
+            enableLanguageFeature(LanguageFeature.MultiDollarInterpolation.name)
+            enableLanguageFeature(LanguageFeature.BreakContinueInInlineLambdas.name)
+        }
     }
 }
 
