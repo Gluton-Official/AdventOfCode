8c4eeeefe596b1d3325880197da8c8c774cb24b4
Gluton <glutonofficial@gmail.com>
2024-12-03 01:20:55 -0500

    Clean up Day 3 solutions

--- src/aoc2024/Day03.kt
+++ src/aoc2024/Day03.kt
@@ -10,11 +10,11 @@ object Day03 : AoCPuzzle() {
         """.trimIndent()),
 	)
 
-    val mulInstructions = """mul\((?<a>\d{1,3}),(?<b>\d{1,3})\)""".toRegex()
 
     override fun part1(input: Input): Int {
+        val mul = """mul\((\d{1,3}),(\d{1,3})\)""".toRegex()
         return input.sumOf {
-            mulInstructions.findAll(it).map {
+            mul.findAll(it).map {
                 it.groupValues.drop(1).let { (a, b) -> a.toInt() * b.toInt() }
             }.sum()
         }
@@ -27,6 +27,7 @@ object Day03 : AoCPuzzle() {
 	)
 
     override fun part2(input: Input): Int {
+        val mul = """mul\((\d{1,3}),(\d{1,3})\)""".toRegex()
         val enable = """do\(\)""".toRegex()
         val disable = """don't\(\)""".toRegex()
         var enabled = true
@@ -34,13 +35,16 @@ object Day03 : AoCPuzzle() {
             var sum = 0
             var pos = 0
             while (pos < it.length) {
-                val next = listOf(enable.find(it, pos), disable.find(it, pos), mulInstructions.find(it, pos)).minBy { it?.range?.first ?: Int.MAX_VALUE } ?: break
-                if (next.value.startsWith("don't")) {
-                    enabled = false
-                } else if (next.value.startsWith("do")) {
-                    enabled = true
-                } else if (enabled) {
-                    sum += next.groupValues.drop(1).let { (a, b) -> a.toInt() * b.toInt() }
+                val next = listOf(
+                    enable.find(it, pos),
+                    disable.find(it, pos),
+                    mul.find(it, pos)
+                ).minBy { it?.range?.first ?: Int.MAX_VALUE } ?: break
+
+                when {
+                    next.value.startsWith("don't") -> enabled = false
+                    next.value.startsWith("do") -> enabled = true
+                    enabled -> sum += next.groupValues.drop(1).let { (a, b) -> a.toInt() * b.toInt() }
                 }
                 pos = next.range.last
             }
