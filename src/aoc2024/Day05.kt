c05be0b08485ca744e8c92f11aab811ef4345fb4
Gluton <glutonofficial@gmail.com>
2024-12-05 00:47:15 -0500

    Initial Day 5 solution

---
+++ src/aoc2024/Day05.kt
@@ -0,0 +1,133 @@
+package aoc2024
+
+import AoCPuzzle
+import jdk.javadoc.internal.doclets.formats.html.markup.HtmlStyle.index
+import util.Input
+import util.toPair
+import java.util.random.RandomGeneratorFactory.all
+
+object Day05 : AoCPuzzle() {
+    override val part1Tests = listOf(
+		Test(143, """
+            47|53
+            97|13
+            97|61
+            97|47
+            75|29
+            61|13
+            75|53
+            29|13
+            97|29
+            53|29
+            61|53
+            97|53
+            61|29
+            47|13
+            75|47
+            97|75
+            47|61
+            75|61
+            47|29
+            75|13
+            53|13
+
+            75,47,61,53,29
+            97,61,53,29,13
+            75,29,13
+            75,97,47,61,53
+            61,13,29
+            97,13,75,29,47
+        """.trimIndent()),
+	)
+
+    override fun part1(input: Input): Int {
+        val rules = input.takeWhile(String::isNotBlank).map { it.split('|').map(String::toInt).toPair() }.groupBy(
+            keySelector = { it.first },
+            valueTransform = { it.second}
+        )
+        val updates = input.takeLastWhile(String::isNotBlank).map { it.split(',').map(String::toInt) }
+
+        return updates.filter { update ->
+            update.withIndex().reversed().all { (index, page) ->
+                update.take(index).none { otherPage ->
+                    otherPage in (rules[page] ?: return@none false)
+                }
+            }
+        }.sumOf {
+            it[it.indices.last / 2]
+        }
+    }
+
+    override val part2Tests = listOf(
+		Test(123, """
+            47|53
+            97|13
+            97|61
+            97|47
+            75|29
+            61|13
+            75|53
+            29|13
+            97|29
+            53|29
+            61|53
+            97|53
+            61|29
+            47|13
+            75|47
+            97|75
+            47|61
+            75|61
+            47|29
+            75|13
+            53|13
+
+            75,47,61,53,29
+            97,61,53,29,13
+            75,29,13
+            75,97,47,61,53
+            61,13,29
+            97,13,75,29,47
+        """.trimIndent()),
+	)
+
+    override fun part2(input: Input): Int {
+        val rules = input.takeWhile(String::isNotBlank).map { it.split('|').map(String::toInt).toPair() }.groupBy(
+            keySelector = { it.first },
+            valueTransform = { it.second}
+        )
+        val updates = input.takeLastWhile(String::isNotBlank).map { it.split(',').map(String::toInt) }
+
+        return updates.filterNot { update ->
+            update.withIndex().reversed().all { (index, page) ->
+                update.take(index).none { otherPage ->
+                    otherPage in (rules[page] ?: return@none false)
+                }
+            }
+        }.map { it.toMutableList() }.onEach { update ->
+            for (i in update.lastIndex downTo 1) {
+                for (j in (i - 1) downTo 0) {
+                    val page = update[i]
+                    val otherPage = update[j]
+                    if (otherPage in rules[page].orEmpty()) {
+                        update[j] = page
+                        update[i] = otherPage
+                    }
+                }
+            }
+        }.sumOf {
+            it[it.indices.last / 2]
+        }
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        if (testPart1()) {
+            runPart1()
+        }
+
+        if (testPart2()) {
+            runPart2()
+        }
+    }
+}
