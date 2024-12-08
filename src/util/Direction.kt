0ec0f1437a257686ae347a1770716281ec1f23fa
Gluton <glutonofficial@gmail.com>
2024-12-07 23:59:40 -0500

    Expand Direction library

--- src/util/Direction.kt
+++ src/util/Direction.kt
@@ -6,11 +6,43 @@ import kotlin.math.absoluteValue
 enum class Direction {
     North, East, South, West;
 
+    fun isVertical(): Boolean = when (this) {
+        North, South -> true
+        else -> false
+    }
+
+    fun isHorizontal(): Boolean = when (this) {
+        East, West -> true
+        else -> false
+    }
+
+    @Deprecated("", replaceWith = ReplaceWith("opposite()"))
     val opposite: Direction get() = when (this) {
         North -> South
         South -> North
         East -> West
         West -> East
+    }
+
+    fun opposite(): Direction = when (this) {
+        North -> South
+        South -> North
+        East -> West
+        West -> East
+    }
+
+    fun rotateClockwise(): Direction = when (this) {
+        North -> East
+        East -> South
+        South -> West
+        West -> North
+    }
+
+    fun rotateCounterClockwise(): Direction = when (this) {
+        North -> West
+        West -> South
+        South -> East
+        East -> North
     }
 
     fun onAxisOf(other: Direction): Boolean = this == other || this == other.opposite
