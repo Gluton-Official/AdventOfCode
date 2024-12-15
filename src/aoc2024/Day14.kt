be57f935fea4c78d1b9fea88fb60fa177ce1dee4
Gluton <glutonofficial@gmail.com>
2024-12-14 22:48:36 -0500

    Scuffed Day 14 Solution

---
+++ src/aoc2024/Day14.kt
@@ -0,0 +1,148 @@
+package aoc2024
+
+import AoCPuzzle
+import util.Input
+import util.Position
+import util.Vector2
+import util.compareTo
+import util.div
+import util.get
+import util.minus
+import util.mod
+import util.plus
+import util.prettyToString2D
+import util.rem
+import util.set
+import util.setAllAs
+import util.times
+import kotlin.math.log
+
+object Day14 : AoCPuzzle() {
+    override val part1Tests = listOf(
+        Test(0L, """
+            p=2,4 v=2,-3
+        """.trimIndent()),
+		Test(12L, """
+            p=0,4 v=3,-3
+            p=6,3 v=-1,-3
+            p=10,3 v=-1,2
+            p=2,0 v=2,-1
+            p=0,0 v=1,3
+            p=3,0 v=-2,-2
+            p=7,6 v=-1,-3
+            p=3,0 v=-1,-2
+            p=9,3 v=2,3
+            p=7,3 v=-1,2
+            p=2,4 v=2,-3
+            p=9,5 v=-3,-3
+        """.trimIndent()),
+	)
+
+    private data class Robot(val position: Vector2<Long>, val velocity: Vector2<Long>) {
+        companion object {
+            private val inputRegex = """p=(\d+),(\d+) v=(-?\d+),(-?\d+)""".toRegex()
+
+            fun from(input: String): Robot = inputRegex.find(input)!!.destructured.let { (px, py, vx, vy) ->
+                Robot(Vector2(px.toLong(), py.toLong()), Vector2(vx.toLong(), vy.toLong()))
+            }
+        }
+    }
+
+    private val TEST_BATHROOM_SIZE = Vector2(11L, 7L)
+    private val BATHROOM_SIZE = Vector2(101L, 103L)
+
+    override fun part1(input: Input): Long {
+        val robots = input.map(Robot::from)
+        val bathroomSize = BATHROOM_SIZE
+        val seconds = 100L
+
+        val finalRobotPositions = robots.map { robot ->
+            (robot.position + robot.velocity * seconds).mod(bathroomSize)
+        }
+
+        val halfBathroomSize = bathroomSize / 2
+        var (quad1, quad2, quad3, quad4) = listOf(0L, 0L, 0L, 0L)
+        finalRobotPositions.forEach {
+            when {
+                it.x < halfBathroomSize.x && it.y < halfBathroomSize.y -> quad1++
+                it.x > halfBathroomSize.x && it.y < halfBathroomSize.y -> quad2++
+                it.x < halfBathroomSize.x && it.y > halfBathroomSize.y -> quad3++
+                it.x > halfBathroomSize.x && it.y > halfBathroomSize.y -> quad4++
+            }
+        }
+
+        List(bathroomSize.y.toInt()) { MutableList<String>(bathroomSize.x.toInt()) { "." } }
+            .also { bathroom ->
+                finalRobotPositions.forEach {
+                    val pos = Position(it.y.toInt(), it.x.toInt())
+                    bathroom[pos] = when (val string = bathroom[pos]) {
+                        "." -> "1"
+                        else -> (string.toInt() + 1).toString()
+                    }
+                }
+            }
+            .prettyToString2D()
+            .also { kotlin.io.println(it) }
+        println(listOf(quad1, quad2, quad3, quad4))
+
+        return quad1 * quad2 * quad3 * quad4
+    }
+
+    override val part2Tests = listOf(
+		Test(0, """
+            
+        """.trimIndent()),
+	)
+
+    override fun part2(input: Input): Int {
+        val robots = input.map(Robot::from)
+        val bathroomSize = BATHROOM_SIZE
+
+//        while (true) {
+//            var minEntropy = Float.MAX_VALUE to 0
+//            repeat(10000) { seconds ->
+        val seconds = 6577
+                val finalRobotPositions = robots.map { robot ->
+                    (robot.position + robot.velocity * seconds.toLong()).mod(bathroomSize)
+                }
+
+        List(bathroomSize.y.toInt()) { MutableList<String>(bathroomSize.x.toInt()) { "." } }
+            .also { bathroom ->
+                finalRobotPositions.forEach {
+                    val pos = Position(it.y.toInt(), it.x.toInt())
+                    bathroom[pos] = when (val string = bathroom[pos]) {
+                        "." -> "1"
+                        else -> (string.toInt() + 1).toString()
+                    }
+                }
+            }
+            .prettyToString2D()
+            .also { kotlin.io.println(it) }
+
+                val robotMap = finalRobotPositions.groupingBy { it }.eachCount()
+
+                val entropy = robotMap.values.fold(0f) { acc, robots ->
+                    acc + robots * log(robots.toFloat(), 2f)
+                }
+
+//                if (entropy < minEntropy.first) {
+//                    minEntropy = entropy to seconds
+//                }
+//            }
+//            println(minEntropy.second)
+//        }
+
+        return 0
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+//        if (testPart1()) {
+//            runPart1()
+//        }
+
+//        if (testPart2()) {
+            runPart2()
+//        }
+    }
+}
--- src/util/Vector.kt
+++ src/util/Vector.kt
@@ -1,3 +1,44 @@
 package util
 
-data class Vector2<T : Number>(val x: T, val y: T)
+import kotlin.math.sqrt
+
+data class Vector2<T : Number>(val x: T, val y: T) {
+    val magnitude: Double by lazy {
+        sqrt(x.toDouble() * x.toDouble() + y.toDouble() * y.toDouble())
+    }
+}
+
+@JvmName("plusVector2Ints")
+operator fun Vector2<Int>.plus(other: Vector2<Int>): Vector2<Int> = Vector2(x + other.x, y + other.y)
+@JvmName("plusVector2Longs")
+operator fun Vector2<Long>.plus(other: Vector2<Long>): Vector2<Long> = Vector2(x + other.x, y + other.y)
+
+@JvmName("minusVector2Ints")
+operator fun Vector2<Int>.minus(other: Vector2<Int>): Vector2<Int> = Vector2(x - other.x, y - other.y)
+@JvmName("minusVector2Longs")
+operator fun Vector2<Long>.minus(other: Vector2<Long>): Vector2<Long> = Vector2(x - other.x, y - other.y)
+
+@JvmName("timesVector2Int")
+operator fun Vector2<Int>.times(scalar: Int): Vector2<Int> = Vector2(x * scalar, y * scalar)
+@JvmName("timesVector2Long")
+operator fun Vector2<Long>.times(scalar: Long): Vector2<Long> = Vector2(x * scalar, y * scalar)
+
+@JvmName("divVector2Int")
+operator fun Vector2<Int>.div(scalar: Int): Vector2<Int> = Vector2(x / scalar, y / scalar)
+@JvmName("divVector2Long")
+operator fun Vector2<Long>.div(scalar: Long): Vector2<Long> = Vector2(x / scalar, y / scalar)
+
+@JvmName("remVector2Ints")
+operator fun Vector2<Int>.rem(other: Vector2<Int>): Vector2<Int> = Vector2(x % other.x, y % other.y)
+@JvmName("remVector2Longs")
+operator fun Vector2<Long>.rem(other: Vector2<Long>): Vector2<Long> = Vector2(x % other.x, y % other.y)
+
+@JvmName("modVector2Ints")
+fun Vector2<Int>.mod(other: Vector2<Int>): Vector2<Int> = Vector2(x.mod(other.x), y.mod(other.y))
+@JvmName("modVector2Longs")
+fun Vector2<Long>.mod(other: Vector2<Long>): Vector2<Long> = Vector2(x.mod(other.x), y.mod(other.y))
+
+@JvmName("compareVector2Ints")
+operator fun Vector2<Int>.compareTo(other: Vector2<Int>): Int = magnitude.compareTo(other.magnitude)
+@JvmName("compareVector2Longs")
+operator fun Vector2<Long>.compareTo(other: Vector2<Long>): Int = magnitude.compareTo(other.magnitude)
