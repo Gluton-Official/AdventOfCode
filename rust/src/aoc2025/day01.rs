5d74b3b29f751011ba985b15b50a7e5bed982c5b
Gluton <glutonofficial@gmail.com>
2025-12-02 19:50:50 -0500

    Fix Day 1 warnings

--- rust/src/aoc2025/day01.rs
+++ rust/src/aoc2025/day01.rs
@@ -26,19 +26,18 @@ impl AOCPuzzle<1, 2025> for Day01 {
     }
     fn part1(&self, input: Lines<'_>) -> usize {
         let mut zeros = 0;
-        let distance: i32 = input
-            .fold(50, |acc, line| {
-                let dir = line.chars().next().unwrap();
-                let dis = line.get(1..).unwrap().parse::<i32>().unwrap();
-                let dis = match dir {
-                    'L' => -dis,
-                    'R' => dis,
-                    _ => todo!()
-                };
-                let new = ((acc + dis) + 100) % 100;
-                if new == 0 { zeros += 1; }
-                new
-            });
+        input.fold(50, |acc, line| {
+            let dir = line.chars().next().unwrap();
+            let dis = line.get(1..).unwrap().parse::<i32>().unwrap();
+            let dis = match dir {
+                'L' => -dis,
+                'R' => dis,
+                _ => todo!()
+            };
+            let new = ((acc + dis) + 100) % 100;
+            if new == 0 { zeros += 1; }
+            new
+        });
         zeros
     }
 
@@ -76,32 +75,28 @@ impl AOCPuzzle<1, 2025> for Day01 {
     }
     fn part2(&self, input: Lines<'_>) -> usize {
         let mut zeros = 0;
-        let distance: i32 = input
-             .fold(50, |acc, line| {
-                 let dir = line.chars().next().unwrap();
-                 let dis = line.get(1..).unwrap().parse::<i32>().unwrap();
-                 let dis = match dir {
-                     'L' => -dis,
-                     'R' => dis,
-                     _ => todo!()
-                 };
-                 let mut rotations = if dis.abs() > 100 {
-                     (dis / 100).abs() as usize
-                 } else {
-                     0
-                 };
-                 let new_dis = dis % 100;
-                 let raw = acc + new_dis;
-                 if (new_dis < 0 && raw < 0 && raw > new_dis) || raw > 100 { rotations += 1; }
-                 let new = ((raw % 100) + 100) % 100;
-                 if new == 0 { rotations += 1; }
-                 if acc == 0 && new == 0 && dis != 0 { rotations -= 1; }
-                 if dis.abs() > 100 {
-                     println!("{}--({})->{} ({})", acc, dis, new, rotations);
-                 }
-                 zeros += rotations;
-                 new
-             });
+        input.fold(50, |acc, line| {
+             let dir = line.chars().next().unwrap();
+             let dis = line.get(1..).unwrap().parse::<i32>().unwrap();
+             let dis = match dir {
+                 'l' => -dis,
+                 'r' => dis,
+                 _ => todo!()
+             };
+             let mut rotations = if dis.abs() > 100 {
+                 (dis / 100).abs() as usize
+             } else {
+                 0
+             };
+             let new_dis = dis % 100;
+             let raw = acc + new_dis;
+             if (new_dis < 0 && raw < 0 && raw > new_dis) || raw > 100 { rotations += 1; }
+             let new = ((raw % 100) + 100) % 100;
+             if new == 0 { rotations += 1; }
+             if acc == 0 && new == 0 && dis != 0 { rotations -= 1; }
+             zeros += rotations;
+             new
+         });
         zeros
     }
 }
